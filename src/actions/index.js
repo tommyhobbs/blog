7e5d1021c042b69230ac805d2b111cf40b001a33
Tom Hobbs <TomHobbs@Outlook.com>
2019-04-23 14:14:37 +0100

    Complete section 15. No loadash used.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,25 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const userIds = Array.from(
+    new Set(getState().posts.map(post => post.userId))
+  );
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
   dispatch({
     type: 'FETCH_POSTS',
-    payload: response
+    payload: response.data
+  });
+};
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({
+    type: 'FETCH_USER',
+    payload: response.data
   });
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,9 @@
 import { combineReducers } from 'redux';
 
+import postsReducer from './postsReducer';
+import usersReducer from './usersReducer';
+
 export default combineReducers({
-  replaceMe: () => 'hello'
+  posts: postsReducer,
+  users: usersReducer
 });
